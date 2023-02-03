cf5242117bafc822aa9cecc6a7ddf3cfa63e5045
alihamdan2020 <ali_hamdane@hotmail.com>
2023-02-03 17:12:15 +0200

    computed and validation with using v-model

---
+++ myComponents.vue
@@ -0,0 +1,9 @@
+Vue.component('MyItem',{
+data : function() {
+return  {
+	message : "$"
+	}
+},
+template : 	"<div  class='item'><img :src=src>   <p>{{name}}</p><p>{{price}} {{message}}</p><a :href=id>click hrere</a></div>",
+props : ['name','price','id','src']
+})
